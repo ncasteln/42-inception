8cc8ec3a9dda3bbbc691e980ad7b373633cbdd07
Nicolo Castelnuovo <ncasteln@3-B-10.42heilbronn.de>
2024-06-03 14:11:19 +0200

    Small changes to create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -5,14 +5,14 @@ mkdir -p ./secrets/mariadb ./secrets/wordpress;
 echo -e "\
 DB_USER=\n\
 DB_PASSWORD=\n\
-DB_NAME=\n\
+DB_NAME=wp_\n\
 DB_ROOT_PASSWORD=\n" > ./secrets/mariadb/.env;
 
 echo -e "\
 DB_USER=\n\
 DB_PASSWORD=\n\
-DB_NAME=\n\
-DB_HOST=\n\
+DB_NAME=wp_\n\
+DB_HOST=mariadb:3306\n\
 ADMIN_USER=\n\
 ADMIN_PASSWORD=\n\
 ADMIN_EMAIL=\n" > ./secrets/wordpress/.env;
